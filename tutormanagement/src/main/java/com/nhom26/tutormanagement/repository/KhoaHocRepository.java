f1145579a583d8cc1410687a32e8ead450af697c
TruongVanTrang <vantrang04042005@gmail.com>
2026-05-06 20:05:03 +0700

    pull og develop

--- tutormanagement/src/main/java/com/nhom26/tutormanagement/repository/KhoaHocRepository.java
+++ tutormanagement/src/main/java/com/nhom26/tutormanagement/repository/KhoaHocRepository.java
@@ -3,6 +3,8 @@ package com.nhom26.tutormanagement.repository;
 import com.nhom26.tutormanagement.entity.KhoaHoc;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query; 
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -16,4 +18,6 @@ public interface KhoaHocRepository extends JpaRepository<KhoaHoc, String> {
     // Tìm kiếm kết hợp (Môn học, Lớp, Mức giá tối đa)
     List<KhoaHoc> findByMonHoc_IdMonHocAndDanhMucLop_IdDanhMucLopAndSoTienHocLessThanEqual(
             String idMonHoc, String idDanhMucLop, BigDecimal maxPrice);
+     @Query("SELECT MAX(k.idKhoaHoc) FROM KhoaHoc k")
+    String findMaxId();
 }
